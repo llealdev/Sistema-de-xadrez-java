b3a2347bf6b5b827c0b34cabe498d81a1b2c950f
Matheus Leal <lleal.dev@gmail.com>
2024-04-07 11:00:10 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Board.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package boardgame;
+
+
+public class Board {
+    
+    private int rows;
+    private int columns;
+    private Piece[][] pieces;
+
+    public Board(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        pieces = new Piece[rows][columns];
+    }
+    
+    
+}
